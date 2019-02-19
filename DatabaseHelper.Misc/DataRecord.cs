cef5677be91daff6ee44f860ae625167f3f713c3
samuel grahame <samuel@intelogy.com.au>
2019-02-19 14:54:56 +1100

    added database misc reader

---
+++ DatabaseHelper.Misc/DataRecord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseHelper.Misc
+{
+    public class DataRecord
+    {
+        internal int RowIndex;
+        public DataResults parent;
+
+        public static implicit operator bool(DataRecord dataRecord)
+        {
+            return dataRecord != null;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                var result = parent.List[RowIndex + index];
+                if (result == DBNull.Value)
+                    return null;
+                return result;
+            }
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                var index = parent.NameIndex[name];
+                var result = parent.List[RowIndex + index];
+                if (result == DBNull.Value)
+                    return null;
+                return result;
+            }
+        }
+    }
+}
---
+++ DatabaseHelper.Misc/DataResults.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseHelper.Misc
+{
+    public class DataResults
+    {
+        private long _numberRows;
+        public long NumberRows => _numberRows;
+
+        public Dictionary<string, int> NameIndex = new Dictionary<string, int>();
+
+        public List<object> List;
+
+        public static DataResults GetDataResults(IDataReader reader, int expectedRows = 1)
+        {
+            var dataResults = new DataResults();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                dataResults.NameIndex.Add(reader.GetName(i), i);
+            }
+            if (reader is DbDataReader dataReader)
+                if (!dataReader.HasRows)
+                    return dataResults;
+
+            int colCount = reader.FieldCount;
+
+            if (expectedRows <= 0)
+                expectedRows = 1;
+
+            dataResults.List = new List<object>(expectedRows * colCount);
+
+            while (reader.Read())
+            {
+                int index = dataResults.List.Count;
+                for (int i = 0; i < colCount; i++)
+                {
+                    dataResults.List.Add(reader.GetValue(i));
+                }
+                dataResults.mysqlRows.AddLast(new DataRecord() { RowIndex = index, parent = dataResults });
+            }
+            dataResults._numberRows = dataResults.mysqlRows.Count;
+            dataResults.currentRow = dataResults.mysqlRows.First;
+
+            return dataResults;
+        }
+        public LinkedList<DataRecord> mysqlRows = new LinkedList<DataRecord>();
+        private LinkedListNode<DataRecord> currentRow = null;
+
+        public DataRecord FetchAssoc()
+        {
+            return (currentRow = currentRow.Next)?.Value;
+        }
+
+        public static implicit operator bool(DataResults dataResults)
+        {
+            return dataResults != null;
+        }
+    }
+}
